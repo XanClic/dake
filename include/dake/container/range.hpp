094d6f6cfaa4ddd38cc20f3bb6c0b3a4c7feadcd
Max Reitz <xanclic@gmail.com>
2014-01-23 21:07:45 +0100

    container/range
    + Add range class.

---
+++ include/dake/container/range.hpp
@@ -0,0 +1,82 @@
+#ifndef DAKE__CONTAINER__RANGE_HPP
+#define DAKE__CONTAINER__RANGE_HPP
+
+#include <iterator>
+
+
+namespace dake
+{
+namespace container
+{
+
+template<typename T> class range;
+
+template<typename T>
+class range_iterator
+{
+    private:
+        T pos;
+
+        range_iterator(const T &initial):
+            pos(initial)
+        {}
+
+    public:
+        typedef T difference_type;
+        typedef T value_type;
+        typedef T *pointer;
+        typedef T &reference;
+        typedef std::bidirectional_iterator_tag iterator_category;
+
+
+        T operator*(void)
+        { return pos; }
+
+        T *operator->(void)
+        { return &pos; }
+
+        bool operator!=(const range_iterator &ri)
+        { return ri.pos != pos; }
+
+        range_iterator &operator++(void)
+        { pos++; return *this; }
+
+        range_iterator operator++(int _)
+        { (void)_; return range_iterator<T>(pos++); }
+
+        range_iterator &operator--(void)
+        { pos--; return *this; }
+
+        range_iterator operator--(int _)
+        { (void)_; return range_iterator<T>(pos--); }
+
+
+    friend class range<T>;
+};
+
+
+template<typename T = int>
+class range
+{
+    private:
+        T s, e;
+
+    public:
+        range(const T &start, const T &end):
+            s(start), e(end)
+        { e++; }
+
+        range_iterator<T> begin(void) const
+        { return range_iterator<T>(s); }
+
+        range_iterator<T> end(void) const
+        { return range_iterator<T>(e); }
+
+
+    friend class range_iterator<T>;
+};
+
+}
+}
+
+#endif
