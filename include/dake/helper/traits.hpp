e83f1e244867d15433a903d63736fb88ea2f5617
Hanna Reitz <xanclic@xanclic.moe>
2014-05-17 18:01:20 +0200

    helper/traits: Fix has_X functions
    ! Explicitly specify the function call for the _def_member_test macro,
      so has_begin and has_end actually work

--- include/dake/helper/traits.hpp
+++ include/dake/helper/traits.hpp
@@ -8,19 +8,20 @@ namespace dake
 namespace helper
 {
 
-#define _def_member_test(name) \
+#define _def_member_test(name, funcall) \
     template<typename T> struct has_##name \
     { \
         template<typename U> static char (&_(...))[2]; \
-        template<typename U> static char _(decltype(std::declval<U &>().name(std::declval<typename U::value_type>())) *); \
+        template<typename U> static char _(decltype(std::declval<U &>().funcall) *); \
         static bool const value = sizeof(_<T>(nullptr)) == sizeof(char); \
     }
 
-_def_member_test(push);
-_def_member_test(push_back);
-_def_member_test(insert);
-_def_member_test(begin);
-_def_member_test(end);
+_def_member_test(push,      push     (std::declval<typename U::value_type>()));
+_def_member_test(push_back, push_back(std::declval<typename U::value_type>()));
+_def_member_test(insert,    insert   (std::declval<typename U::value_type>()));
+
+_def_member_test(begin, begin());
+_def_member_test(end,   end  ());
 
 template<typename T> struct is_pushable { static const bool value = has_push_back<T>::value || has_push<T>::value; };
 template<typename T> struct is_iterable { static const bool value = has_begin<T>::value && has_end<T>::value; };
