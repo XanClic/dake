bb224f70946a8aa6789073e8bc436b32f4bd2a30
Max Reitz <mreitz@redhat.com>
2014-05-24 20:38:35 +0200

    extern "C"
    + around the OpenGL headers

--- include/dake/gl/gl.hpp
+++ include/dake/gl/gl.hpp
@@ -2,17 +2,24 @@
 #define DAKE__GL__GL_HPP
 
 #ifdef __MINGW32__
+extern "C" {
 #include <GL/glew.h>
+}
+
 namespace dake { namespace gl { static inline bool glext_init(void) { return glewInit() == GLEW_OK; } } }
 #else
 #define GL_GLEXT_PROTOTYPES
 namespace dake { namespace gl { static inline bool glext_init(void) { return true; } } }
 #endif
 
+extern "C" {
 #include <GL/gl.h>
+}
 
 #ifndef __MINGW32__
+extern "C" {
 #include <GL/glext.h>
+}
 #endif
 
 #endif
