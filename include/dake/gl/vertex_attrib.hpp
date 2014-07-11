9eb289c5a31d0610c50bd90bd0ceb206c5646c66
Max Reitz <xanclic@gmail.com>
2014-07-11 20:12:30 +0200

    gl/vertex_attrib: Small fixes
    ! Remove *vattr from header: Missing forward declaration and was
      useless anyway
    ! Accept const void * as buffer for gl::vertex_attrib::data()
    ! Bind appropriate gl::vertex_array in gl::vertex_attrib::bind()

--- include/dake/gl/vertex_attrib.hpp
+++ include/dake/gl/vertex_attrib.hpp
@@ -15,8 +15,6 @@ namespace gl
 
 class vertex_array;
 
-extern vertex_attrib *vattr;
-
 class vertex_attrib {
     private:
         GLuint buffer, attrib;
@@ -40,7 +38,7 @@ class vertex_attrib {
 
         void load(size_t stride = 0, uintptr_t offset = 0);
 
-        void data(void *ptr, size_t size = static_cast<size_t>(-1), GLenum usage = GL_STATIC_DRAW);
+        void data(const void *ptr, size_t size = static_cast<size_t>(-1), GLenum usage = GL_STATIC_DRAW);
 
         void *map(bool readable = false);
         void unmap(void);
