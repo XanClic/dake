1fea137cf5d469ae207efb8a54c22cff41c6f9fb
Max Reitz <xanclic@gmail.com>
2014-07-27 23:11:40 +0200

    Headers: Add collective headers

---
+++ include/dake/gl.hpp
@@ -0,0 +1,14 @@
+#ifndef DAKE__GL_HPP
+#define DAKE__GL_HPP
+
+#include "dake/gl/elements_array.hpp"
+#include "dake/gl/find_resource.hpp"
+#include "dake/gl/framebuffer.hpp"
+#include "dake/gl/gl.hpp"
+#include "dake/gl/obj.hpp"
+#include "dake/gl/shader.hpp"
+#include "dake/gl/texture.hpp"
+#include "dake/gl/vertex_array.hpp"
+#include "dake/gl/vertex_attrib.hpp"
+
+#endif
---
+++ include/dake/math.hpp
@@ -0,0 +1,6 @@
+#ifndef DAKE__MATH_HPP
+#define DAKE__MATH_HPP
+
+#include "dake/math/matrix.hpp"
+
+#endif
