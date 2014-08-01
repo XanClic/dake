17f0fb5a08c0917c2bd2f854f98d5dec2e904797
Max Reitz <xanclic@gmail.com>
2014-08-01 20:59:22 +0200

    meh always move stuff me non gusta

--- include/dake/helper/function.hpp
+++ include/dake/helper/function.hpp
@@ -1,70 +1,29 @@
 #ifndef DAKE__HELPER__FUNCTION_HPP
 #define DAKE__HELPER__FUNCTION_HPP
 
+#include <utility>
+
+
 namespace dake
 {
 namespace helper
 {
 
-template<typename T>
-T sum(const T &x, const T &y)
-{
-    return x + y;
-}
+#define _generate_helper(name, nf_code, f_code) \
+    template<typename U, typename V> auto name(const U &x, const V &y) -> decltype(nf_code) { return nf_code; } \
+    template<typename U, typename V> auto name(U &&x, V &&y) -> decltype(f_code) { return f_code; }
 
+_generate_helper(sum,                   x + y, std::forward(x) + std::forward(y));
+_generate_helper(difference,            x - y, std::forward(x) - std::forward(y));
+_generate_helper(product,               x * y, std::forward(x) * std::forward(y));
+_generate_helper(quotient,              x / y, std::forward(x) / std::forward(y));
+_generate_helper(conjunction,           x & y, std::forward(x) & std::forward(y));
+_generate_helper(disjunction,           x | y, std::forward(x) | std::forward(y));
+_generate_helper(exclusive_disjunction, x ^ y, std::forward(x) ^ std::forward(y));
+_generate_helper(maximum,               (x > y) ? x : y, (std::forward(x) > std::forward(y)) ? std::forward(x) : std::forward(y));
+_generate_helper(minimum,               (x < y) ? x : y, (std::forward(x) < std::forward(y)) ? std::forward(x) : std::forward(y));
 
-template<typename T>
-T difference(const T &x, const T &y)
-{
-    return x - y;
-}
-
-
-template<typename T>
-T product(const T &x, const T &y)
-{
-    return x * y;
-}
-
-
-template<typename T>
-T quotient(const T &x, const T &y)
-{
-    return x / y;
-}
-
-
-template<typename T>
-T conjunction(const T &x, const T &y)
-{
-    return x & y;
-}
-
-
-template<typename T>
-T disjunction(const T &x, const T &y)
-{
-    return x | y;
-}
-
-
-template<typename T>
-T exclusive_disjunction(const T &x, const T &y)
-{
-    return x ^ y;
-}
-
-template<typename T>
-const T &maximum(const T &x, const T &y)
-{
-    return (x > y) ? x : y;
-}
-
-template<typename T>
-const T &minimum(const T &x, const T &y)
-{
-    return (x < y) ? x : y;
-}
+#undef _generate_helper
 
 }
 }
