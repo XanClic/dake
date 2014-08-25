39f3f37b181efa48a508ec6d8bec0d6cf6f9516e
Max Reitz <xanclic@gmail.com>
2014-08-25 22:07:58 +0200

    gl/stubs: Add glVertexAttribLPointer()

--- lib/gl/stubs.cpp
+++ lib/gl/stubs.cpp
@@ -10,6 +10,7 @@ GLuint64 glGetTextureHandleARB(GLuint texture) __attribute__((weak));
 void glMakeTextureHandleResidentARB(GLuint64 handle) __attribute__((weak));
 void glMakeTextureHandleNonResidentARB(GLuint64 handle) __attribute__((weak));
 void glUniformHandleui64ARB(GLint location, GLuint64 value) __attribute__((weak));
+void glVertexAttribLPointer(GLuint index, GLint size, GLenum type, GLsizei stride, const void *pointer) __attribute__((weak));
 };
 
 GLuint64 glGetTextureHandleARB(GLuint texture)
@@ -35,6 +36,16 @@ void glUniformHandleui64ARB(GLint location, GLuint64 value)
     (void)location;
     (void)value;
     throw std::runtime_error("glUniformHandleui64ARB() not available");
+}
+
+void glVertexAttribLPointer(GLuint index, GLint size, GLenum type, GLsizei stride, const void *pointer)
+{
+    (void)index;
+    (void)size;
+    (void)type;
+    (void)stride;
+    (void)pointer;
+    throw std::runtime_error("glVertexAttribLPointer() not available");
 }
 
 #endif
