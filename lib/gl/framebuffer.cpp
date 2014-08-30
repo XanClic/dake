86ea7eb8ffecdd413858269aba49af06014aaa90
Max Reitz <xanclic@gmail.com>
2014-08-30 22:33:10 +0200

    gl/framebuffer: Stencil buffer support (untested)
    + Untested, because using both a stencil and a depth attachment do not
      work; creating a texture view from a combined stencil/depth buffer
      does not work either; and using both the stencil and the depth part at
      the same time is impossible as well (thanks based ARB)
    
    “there's no great need” — ARB_texture_view

--- lib/gl/framebuffer.cpp
+++ lib/gl/framebuffer.cpp
@@ -1,3 +1,5 @@
+#include <stdexcept>
+
 #include <dake/gl/framebuffer.hpp>
 
 
@@ -14,9 +16,18 @@ dake::gl::framebuffer *current_fb = nullptr;
 }
 
 
-dake::gl::framebuffer::framebuffer(int color_attachments, GLenum format):
-    ca_count(color_attachments)
+dake::gl::framebuffer::framebuffer(int color_attachments, GLenum format, depth_stencil_mode ds_mode):
+    ca_count(color_attachments),
+    dsm(ds_mode)
 {
+    if ((dsm == STENCIL_XOR_DEPTH) && !glext.has_extension(STENCIL_TEXTURING)) {
+        dsm = STENCIL_AND_DEPTH;
+    }
+
+    if (dsm == STENCIL_AND_DEPTH) {
+        throw std::invalid_argument("Depth/stencil mode STENCIL_AND_DEPTH is not supported");
+    }
+
     draw_buffers = new GLenum[ca_count];
     for (int i = 0; i < ca_count; i++) {
         draw_buffers[i] = GL_COLOR_ATTACHMENT0 + i;
@@ -32,6 +43,27 @@ dake::gl::framebuffer::framebuffer(int color_attachments, GLenum format):
 
     textures = new texture[ca_count];
 
+    switch (dsm) {
+        case DEPTH_ONLY:
+        case STENCIL_XOR_DEPTH:
+            depth_buffer = new texture;
+            stencil_buffer = nullptr;
+            break;
+
+        case STENCIL_ONLY:
+            depth_buffer = nullptr;
+            stencil_buffer = new texture;
+            break;
+
+        case NO_DEPTH_OR_STENCIL:
+            depth_buffer = nullptr;
+            stencil_buffer = nullptr;
+            break;
+
+        default:
+            throw std::invalid_argument("Invalid value given for depth/stencil mode");
+    }
+
     resize(1024, 1024);
 
     for (int i = 0; i < ca_count; i++) {
@@ -39,14 +71,29 @@ dake::gl::framebuffer::framebuffer(int color_attachments, GLenum format):
         glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + i, GL_TEXTURE_2D, textures[i].get_glid(), 0);
     }
 
-    depth_buffer.filter(GL_NEAREST);
-    glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depth_buffer.get_glid(), 0);
+    if (depth_buffer) {
+        depth_buffer->filter(GL_NEAREST);
+        if (dsm == STENCIL_XOR_DEPTH) {
+            glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_TEXTURE_2D, depth_buffer->get_glid(), 0);
+        } else {
+            glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depth_buffer->get_glid(), 0);
+        }
+    } else if (stencil_buffer) {
+        stencil_buffer->filter(GL_NEAREST);
+        glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_TEXTURE_2D, stencil_buffer->get_glid(), 0);
+    }
+
+    if (glCheckFramebufferStatus(GL_DRAW_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+        throw std::runtime_error("Framebuffer incomplete");
+    }
 }
 
 
 dake::gl::framebuffer::~framebuffer(void)
 {
     glDeleteFramebuffers(1, &id);
+    delete depth_buffer;
+    delete stencil_buffer;
     delete[] textures;
     delete[] formats;
     delete[] draw_buffers;
@@ -61,7 +108,16 @@ void dake::gl::framebuffer::resize(int w, int h)
     for (int i = 0; i < ca_count; i++) {
         textures[i].format(formats[i], w, h, GL_RGBA, GL_FLOAT);
     }
-    depth_buffer.format(GL_DEPTH_COMPONENT24, w, h, GL_DEPTH_COMPONENT, GL_FLOAT);
+
+    if (depth_buffer) {
+        if (dsm == STENCIL_XOR_DEPTH) {
+            depth_buffer->format(GL_DEPTH24_STENCIL8, w, h, GL_DEPTH_COMPONENT, GL_FLOAT);
+        } else {
+            depth_buffer->format(GL_DEPTH_COMPONENT24, w, h, GL_DEPTH_COMPONENT, GL_FLOAT);
+        }
+    } else if (stencil_buffer) {
+        stencil_buffer->format(GL_STENCIL_INDEX8, w, h, GL_STENCIL_INDEX, GL_UNSIGNED_BYTE);
+    }
 }
 
 
@@ -75,7 +131,10 @@ void dake::gl::framebuffer::color_format(int i, GLenum format)
 
 void dake::gl::framebuffer::bind(void)
 {
-    glBindFramebuffer(GL_DRAW_FRAMEBUFFER, id);
+    if (current_fb != this) {
+        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, id);
+    }
+
     glDrawBuffers(ca_count, draw_buffers);
 
     current_fb = this;
@@ -90,7 +149,37 @@ dake::gl::texture &dake::gl::framebuffer::operator[](int i)
 
 dake::gl::texture &dake::gl::framebuffer::depth(void)
 {
-    return depth_buffer;
+    if (!depth_buffer) {
+        throw std::runtime_error("No depth buffer available");
+    }
+
+    if ((dsm == STENCIL_XOR_DEPTH) && depth_buffer_in_stencil_mode) {
+        depth_buffer->bind();
+        glTexParameteri(GL_TEXTURE_2D, GL_DEPTH_STENCIL_TEXTURE_MODE, GL_DEPTH_COMPONENT);
+        depth_buffer_in_stencil_mode = false;
+    }
+
+    return *depth_buffer;
+}
+
+
+dake::gl::texture &dake::gl::framebuffer::stencil(void)
+{
+    if ((dsm == NO_DEPTH_OR_STENCIL) || (dsm == DEPTH_ONLY)) {
+        throw std::runtime_error("No stencil buffer available");
+    }
+
+    if ((dsm == STENCIL_XOR_DEPTH) && !depth_buffer_in_stencil_mode) {
+        depth_buffer->bind();
+        glTexParameteri(GL_TEXTURE_2D, GL_DEPTH_STENCIL_TEXTURE_MODE, GL_STENCIL_INDEX);
+        depth_buffer_in_stencil_mode = true;
+    }
+
+    if (dsm == STENCIL_XOR_DEPTH) {
+        return *depth_buffer;
+    } else {
+        return *stencil_buffer;
+    }
 }
 
 
