765416361fb14fdc437eacdc60eaef8f37cc7686
Max Reitz <xanclic@gmail.com>
2015-11-11 23:18:39 +0100

    math/fmatrix: Statically assert the types' sizes
    + The classes are expected to have certain sizes, so we should make sure
      that this is indeed the case.

--- include/dake/math/fmatrix.hpp
+++ include/dake/math/fmatrix.hpp
@@ -77,6 +77,30 @@ class fvec2d;
 
 #undef DAKE__MATH__FMATRIX_HPP__INSIDE
 
+
+// Every type should have (at least) the size it requires as alignment so that
+// packed vectors work
+
+// Needs to be aligned to eight bytes (TODO: Does it really? Not 16?)
+static_assert(sizeof(fvec2) == 0x08, "sizeof(fvec2) != 0x08");
+
+// Need to be aligned to 16 bytes
+static_assert(sizeof(fvec2d) == 0x10, "sizeof(fvec2d) != 0x10");
+static_assert(sizeof(fvec3)  == 0x10, "sizeof(fvec3) != 0x10");
+static_assert(sizeof(fvec4)  == 0x10, "sizeof(fvec4) != 0x10");
+static_assert(sizeof(fmat3)  == 0x30, "sizeof(fmat3) != 0x30");
+static_assert(sizeof(fmat4)  == 0x40, "sizeof(fmat4) != 0x40");
+
+#ifdef __AVX__
+// Need to be aligned to 32 bytes
+static_assert(sizeof(fvec3d) == 0x20, "sizeof(fvec3d) != 0x20");
+static_assert(sizeof(fvec4d) == 0x20, "sizeof(fvec4d) != 0x20");
+#else
+// Need to be aligned to 16 bytes
+static_assert(sizeof(fvec3d) == 0x20, "sizeof(fvec3d) != 0x20");
+static_assert(sizeof(fvec4d) == 0x20, "sizeof(fvec4d) != 0x20");
+#endif
+
 }
 }
 
